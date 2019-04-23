8354ec726040ebff076a55202ffa40f2c56ce4a3
MeasponeS <958183117@qq.com>
2019-04-23 21:33:40 +0800

    Add TodoList&FontStyle

--- src/pages/schedule/Index.vue
+++ src/pages/schedule/Index.vue
@@ -8,7 +8,14 @@
 
             <div class="focus">
                 <h3>Hey,what's your focus today?</h3>
-                <input type="text">
+                <input type="text" v-model="todoMatter" @keyup="addTodo($event)">
+            </div>
+            <div class="todoBox" v-if="todoList.length > 0">
+                <h3>TodoList</h3>
+                <div class="checkGroup">
+                    <Checkbox v-for="item in todoList">{{item}}</Checkbox>
+                </div>
+
             </div>
         </div>
         <!--<Footer></Footer>-->
@@ -16,16 +23,39 @@
 </template>
 
 <script>
-    import {Carousel,CarouselItem} from 'element-ui'
+    import {Checkbox,CheckboxGroup} from 'element-ui'
     export default {
         name: 'app',
         data: function () {
             return {
-                showNav:false
+                showNav:false,
+                todoList:[],
+                todoMatter:""
+            }
+        },
+        watch:{
+            'todoMatter' :function (val) {
+                let reg = /[\u4e00-\u9fa5]/g;
+                if(reg.test(val)){
+                    this.$alert('建议不用使用中文哦~', {
+                        confirmButtonText: '我知道辣',
+                        showClose:false,
+                        callback: action => {
+                            this.todoMatter = ''
+                        }
+                    });
+                }
             }
         },
         methods: {
-
+            addTodo(e){
+                // 按下enter添加列表
+                if(e.keyCode === 13){
+                    if(this.todoMatter == "") return;
+                    this.todoList.unshift(this.todoMatter)
+                    this.todoMatter = ""
+                }
+            }
         },
         mounted() {
 
@@ -33,7 +63,7 @@
         beforeDestroy: function () {
 
         },
-        components: {Carousel,CarouselItem}
+        components: {CheckboxGroup,Checkbox}
     }
 </script>
 
