5c030e1b019706be3b1b028fc9bd8485cdd716b1
MeasponeS <958183117@qq.com>
2019-05-06 10:52:26 +0800

    Contact Me

--- src/pages/contact/Index.vue
+++ src/pages/contact/Index.vue
@@ -4,6 +4,35 @@
 
         <div class="content">
             To be continued...
+            <div class="littleBox">
+                <el-popover
+                    placement="top-start"
+                    title="Secret Admirer"
+                    width="300"
+                    trigger="hover"
+                    >
+                    <div class="wx">
+                        <img src="./img/wx.png" class="left" alt="">
+                        <div class="right">
+                            <img src="./img/wv.png" alt="">
+                        </div>
+
+                    </div>
+                    <div class="wb">
+                        <img src="./img/weibo.png" alt="">
+                        <a href="https://www.weibo.com/5430551179/profile?topnav=1&wvr=6" target="_blank">@张张张小黑_</a>
+                    </div>
+                    <div class="wb">
+                        <img src="./img/email.png" alt="">
+                        <span>MeasponeS@gmail.com</span>
+                    </div>
+                    <div class="wb">
+                        <img src="./img/github.png" alt="">
+                        <a href="https://github.com/MeasponeS" target="_blank">@MeasponeS</a>
+                    </div>
+                    <el-button slot="reference">Contact Me</el-button>
+                </el-popover>
+            </div>
         </div>
         <Footer></Footer>
     </div>
@@ -22,7 +51,7 @@
 
         },
         mounted() {
-            window.addEventListener('scroll',this.handleScroll)
+
         },
         beforeDestroy: function () {
 
